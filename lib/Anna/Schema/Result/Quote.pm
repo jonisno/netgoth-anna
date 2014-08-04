482475df85697f68bc68de60fb81eb4f16eb3b9f
jonisno <jonis@jonis.no>
2014-08-04 20:44:35 +0100

    Fixed Karma and quote tables

--- lib/Anna/Schema/Result/Quote.pm
+++ lib/Anna/Schema/Result/Quote.pm
@@ -1,14 +1,14 @@
-package Anna::Schema::Result::Karma;
+package Anna::Schema::Result::Quote;
 
 use strict;
 use warnings;
 use base qw/DBIx::Class::Core/;
 
 __PACKAGE__->load_components(qw/InflateColumn::DateTime/);
-__PACKAGE__->table('karma');
-__PACKAGE__->add_columns(qw/id value score/);
+__PACKAGE__->table('quotes');
+__PACKAGE__->add_columns(qw/id added_by quote/);
 __PACKAGE__->add_columns(
-  time => { data_type => 'datetime' }
+  added_on => { data_type => 'datetime' }
 );
 __PACKAGE__->set_primary_key('id');
 
